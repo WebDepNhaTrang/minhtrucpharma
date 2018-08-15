8cc856575c27463c3b5ce34488f4871d0263f713
Thien Pham <chithien175@gmail.com>
2018-08-15 17:02:30 +0700

    theme default

---
+++ resources/views/theme-default/partials/scripts.blade.php
@@ -0,0 +1,22 @@
+<!-- Insert JS here -->
+<!--[if lt IE 9]>
+        <script src="js/html5shiv.min.js"></script>
+        <script src="js/respond.min.js"></script>
+	<![endif]-->
+<!-- Stylesheets -->
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/fontawesome/css/font-awesome.min.css" />
+<link rel="stylesheet" type="text/css" href="css/flaticon/flaticon.css" />
+<link rel="stylesheet" type="text/css" href="css/owl.carousel.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap-select.min.css">
+<link rel="stylesheet" type="text/css" href="css/magnific-popup.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<link class="skin"  rel="stylesheet" type="text/css" href="css/skin/skin-1.css">
+<link  rel="stylesheet" type="text/css" href="css/templete.css">
+
+<!-- Revolution Slider Css -->
+<link rel="stylesheet" type="text/css" href="plugins/revolution/revolution/css/settings.css">
+<!-- Revolution Navigation Style -->
+<link rel="stylesheet" type="text/css" href="plugins/revolution/revolution/css/navigation.css">
+<!-- Google fonts -->
+<link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900|Open+Sans:300,400,600,700,800|Roboto:100,300,400,500,700,900" rel="stylesheet">
